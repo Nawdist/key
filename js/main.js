ec7ec1fb4f0faa6c29f8706ee2ffcce277d67834
Nawdist <78550645+Nawdist@users.noreply.github.com>
2021-02-19 16:54:29 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var clock = document.getElementById("clock").addEventListener('click', function(){
+    window.location.href = "js/date.html"
+});
+var stop = document.getElementById("stop").addEventListener('click', function(){
+    window.location.href = "js/stopWatch.html"
+});
+var key = document.getElementById("keyboard").addEventListener('click', function(){
+    window.location.href = "js/keyboard.html"
+});
